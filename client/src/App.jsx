d2b82ec6d37c03a4227a5da0a734beb221a7a5d1
Vedant Saxena <vedantsaxena@Vedants-MacBook-Air.local>
2024-05-05 08:58:54 +0530

    Navbar functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./lib/Navigation/Navbar";
-
+import Table from "./lib/Table/Table";
+import Map from "./lib/Map/Map";
 const App = () => {
+  const [navToggle, setNavToggle] = useState(true);
   return (
     <div>
-      <Navbar />
+      <Navbar navToggle={navToggle} setNavToggle={setNavToggle} />
+      {navToggle ? <Table /> : <Map />}
     </div>
   );
 };
--- client/src/lib/Navigation/Navbar.jsx
+++ client/src/lib/Navigation/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Navbar.scss";
-const Navbar = () => {
+const Navbar = ({ navToggle, setNavToggle }) => {
   return (
     <div className="nav-parent">
-        <p className="logo">Security Alarm System</p>
+      <p className="logo">Security Alarm System</p>
       <div className="button-parent">
-        <div className="button"> Table</div>
-        <div className="button"> Map</div>
+        <div
+          className="button"
+          onClick={() => {
+            setNavToggle(true);
+          }}
+        >
+          Table
+        </div>
+        <div
+          className="button"
+          onClick={() => {
+            setNavToggle(false);
+          }}
+        >
+          Map
+        </div>
       </div>
     </div>
   );
