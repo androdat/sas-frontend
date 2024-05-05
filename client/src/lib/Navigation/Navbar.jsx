3fb1aa5523c2e9e4390402e3ad6a47d13b72ea06
Vedant Saxena <vedantsaxena@Vedants-MacBook-Air.local>
2024-05-05 08:33:34 +0530

    Navbar ui done

--- client/src/lib/Navigation/Navbar.jsx
+++ client/src/lib/Navigation/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./Navbar.scss";
 const Navbar = () => {
   return (
-    <div className="nav">
-      Navbar
-      <div className="button"> dsdsds</div>
+    <div className="nav-parent">
+        <p className="logo">Security Alarm System</p>
+      <div className="button-parent">
+        <div className="button"> Table</div>
+        <div className="button"> Map</div>
+      </div>
     </div>
   );
 };
