13be77a689c06a74e5a24ad5161d00a1ba6e264c
Vedant Saxena <vedantsaxena@Vedants-MacBook-Air.local>
2024-05-05 11:50:45 +0530

    Added pages for table,map and sockets logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
+import { io } from "socket.io-client";
 import Navbar from "./lib/Navigation/Navbar";
 import Table from "./lib/Table/Table";
 import Map from "./lib/Map/Map";
 const App = () => {
+  var connectionOptions = {
+    "force new connection": true,
+    reconnectionAttempts: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  };
+  const socket = io.connect("http://localhost:4000", connectionOptions);
+  console.log(socket);
   const [navToggle, setNavToggle] = useState(true);
   return (
     <div>
       <Navbar navToggle={navToggle} setNavToggle={setNavToggle} />
-      {navToggle ? <Table /> : <Map />}
+      {navToggle ? <Table socket={socket} /> : <Map />}
     </div>
   );
 };
--- client/src/lib/Table/Table.jsx
+++ client/src/lib/Table/Table.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
-const Table = () => {
+const TableComponent = ({ socket }) => {
+  const [sensorData, setSensorData] = useState([]);
+
+  useEffect(() => {
+    handleGetAllData();
+  }, []);
+
+  useEffect(() => {
+    socket.on("sensorData", (data) => {
+      console.log(data);
+      setSensorData(prevItems => [...prevItems, data]);
+    });
+  }, [socket]);
+
+  const handleGetAllData = async () => {
+    let resp = await axios.get("http://localhost:4000/sas/getAll");
+    console.log(resp.data.sensor);
+    setSensorData(resp.data.sensor);
+  };
+
   return (
-    <div>Table</div>
-  )
-}
+    <div>
+      table
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Latitude</TableCell>
+              <TableCell align="right">Longitude</TableCell>
+              <TableCell align="right">Temperature</TableCell>
+              <TableCell align="right">Fuel</TableCell>
+              <TableCell align="right">Power</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {sensorData?.map((row) => (
+              <TableRow
+                key={row._id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row._id}
+                </TableCell>
+                <TableCell align="right">{row.lat}</TableCell>
+                <TableCell align="right">{row.lng}</TableCell>
+                <TableCell align="right">{row.temp}</TableCell>
+                <TableCell align="right">{row.fuel}</TableCell>
+                <TableCell align="right">{row.power}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
-export default Table
+export default TableComponent;
