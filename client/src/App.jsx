a43b737542cc38629d41956a716525e104ee70c9
Vedant Saxena <vedantsaxena@Vedants-MacBook-Air.local>
2024-05-05 08:18:15 +0530

    Basic react app setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./lib/Navigation/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/lib/Navigation/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.scss";
+const Navbar = () => {
+  return (
+    <div className="nav">
+      Navbar
+      <div className="button"> dsdsds</div>
+    </div>
+  );
+};
+
+export default Navbar;
